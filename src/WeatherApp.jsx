60c48be932251c06127a33f2f92688f525b91e3b
Rivaroni <michaelrivaroni@gmail.com>
2025-09-19 00:13:13 -0700

    Phase 1 completed

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -6,15 +6,25 @@ function WeatherApp(){
         city:"Yorba Linda",
         temp:81
     })
-    const apiKey = "e2a1f488af64426995447698550ed654";
+    const apiKey = "&appid=e2a1f488af64426995447698550ed654";
+    const baseURL = "https://api.openweathermap.org/data/2.5/weather"
+    const metricURL = "&units=imperial"
+    const url = `${baseURL}?q=${weatherData.city}${apiKey}${metricURL}`
 
     useEffect(() => {
-
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setWeatherData(w => ({...w, temp: Math.round(data.main.temp)}))
+            })
     }, [])
 
     return(<>
         <div>
             <h1>{weatherData.city}</h1>
+            <hr></hr>
+            <h2>{weatherData.temp + " °F"}</h2>
         </div>
     </>)
 }
