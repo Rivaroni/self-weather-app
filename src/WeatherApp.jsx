7b2ccbee9a9b3cf7a7c324b18b7ee5931f20ece4
Rivaroni <michaelrivaroni@gmail.com>
2025-09-26 11:32:41 -0700

    New UI and added more weather details

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,34 +1,57 @@
-import React, {useState, useEffect, useRef} from "react"
+import React, {useState, useEffect} from "react"
 
 function WeatherApp(){
 
     const [weatherData, setWeatherData] = useState({
         city:"Yorba Linda",
         temp:-1,
-        desc:"null"
+        feels: -1,
+        desc:"null",
+        humidity: -1,
+        wind: -1,
+        icon:""
     })
     const [city, setCity] = useState("")
     const [loading, setLoading] = useState(true)
+    const [error, setError] = useState("")
+    const [searchCity, setSearchCity] = useState("Yorba Linda")
 
     const apiKey = "&appid=e2a1f488af64426995447698550ed654";
     const baseURL = "https://api.openweathermap.org/data/2.5/weather"
     const metricURL = "&units=imperial"
-    const url = `${baseURL}?q=${weatherData.city}${apiKey}${metricURL}`
+    const url = `${baseURL}?q=${searchCity}${apiKey}${metricURL}`
 
     useEffect(() => {
         fetch(url)
-            .then(response => response.json())
+            .then(response => {
+                if(!response.ok){
+                    throw new Error('City not found')
+                }
+                return response.json()
+            })
             .then(data => {
                 console.log(data);
                 setWeatherData(w => ({...w, 
-                    temp: Math.round(data.main.temp), 
-                    desc: data.weather[0].description
+                    city: data.name,
+                    temp: Math.round(data.main.temp),
+                    feels: Math.round(data.main.feels_like),
+                    humidity: Math.round(data.main.humidity),
+                    wind: data.wind.speed,
+                    desc: data.weather[0].description,
+                    icon: data.weather[0].icon
                 }))
                 setLoading(false)
+                setError("")
             })
-    }, [weatherData.city])
+            .catch(error => {
+                console.log("Error", error)
+                setLoading(false)
+                setError(error.message)
+            })
+    }, [searchCity])
 
     function handleCityInput(e){
+        setError("")
         setCity(e.target.value)
     }
 
@@ -40,29 +63,67 @@ function WeatherApp(){
     }
 
     function addCity(){
-        setLoading(true)
-        setWeatherData(w => ({...w, city: city}))
-        setCity("")
+        if(city.trim() != "")
+        {
+            setLoading(true)
+            setError("")
+            setSearchCity(city)
+            setCity("")
+        }
+
     }
 
     return(<>
         <div>
-            <h1>{weatherData.city}</h1>
-            <hr></hr>
-            <h2>{weatherData.temp + " °F"}</h2>
-            <h2>{weatherData.desc}</h2>
-            <hr></hr>
-            <input
-            type="text"
-            placeholder="Type City Name"
-            value={city}
-            onChange={handleCityInput}
-            onKeyDown={handleKeyDown}
-            />
-            <button
-            onClick={addCity}
-            >Enter</button>
-            <h3>{loading ? "Loading..." : ""}</h3>
+            <h3>Check the weather forecast</h3>
+            <div>
+                <input
+                type="text"
+                placeholder="Type City Name"
+                value={city}
+                onChange={handleCityInput}
+                onKeyDown={handleKeyDown}
+                />
+                <button
+                onClick={addCity}
+                >Enter</button>
+            </div>
+            <div>
+                <input
+                type="radio"
+                name="radioOptions"
+                id="radio1"
+                value="option1"
+                checked
+                />
+                <label>Celsius</label>
+                <input
+                type="radio"
+                name="radioOptions"
+                id="radio2"
+                value="option2"
+                />
+                <label>Farenheit</label>
+            </div>
+            <div>
+                <h2>{weatherData.city}</h2>
+                <h3>Current Temperature: <b>{weatherData.temp + " °F"}</b></h3>
+                <h3>Feels like: <b>{weatherData.feels + " °F"}</b></h3>
+                <h3>Humidity: <b>{weatherData.humidity + " %"}</b></h3>
+                <h3>Wind speed: <b>{weatherData.wind + " mph"}</b></h3>
+                <div>
+                    <img
+                    src={`http://openweathermap.org/img/wn/${weatherData.icon}@2x.png`}
+                    />
+                    <h4>{weatherData.desc}</h4>
+                </div>
+
+            </div>
+            <div>
+                <hr></hr>
+                <h2>{loading ? "Loading..." : ""}</h2>
+                <h2>{error}</h2>
+            </div>
         </div>
     </>)
 }
